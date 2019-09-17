8f089717c159ce54eae99cc74d32fb1557dbceee
Dylan Mestyanek <mestyanekdylan@gmail.com>
2019-09-17 17:49:05 -0500

    set up App layout. imported and render form component in App.js

---
+++ team-builder/src/components/Form.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Form = props => {
+    return (
+        <form>
+            <label>
+                <input />
+            </label>
+        </form>
+    );   
+}
+
+export default Form;
