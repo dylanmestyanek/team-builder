1211f3cfe12170b42db25a292a45ffe72f40f4b5
Dylan Mestyanek <mestyanekdylan@gmail.com>
2019-09-17 18:03:08 -0500

    Added input elements, and labels for name, email, and role. Rendering all inputs in individual input box through user state.

--- team-builder/src/components/Form.js
+++ team-builder/src/components/Form.js
@@ -1,11 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Form = props => {
+    const [user, setUser] = useState({
+        name: '',
+        email: '',
+        role: ''
+    })
+
+    const handleChange = (event) => {
+        event.preventDefault();
+        const { name, value } = event.target;
+        setUser({...user, [name]: value});
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setUser({
+            name: '',
+            email: '',
+            role: ''
+        })
+        console.log(user);
+    };
+ 
     return (
-        <form>
+        <form onSubmit={(event) => handleSubmit(event)}>
             <label>
-                <input />
+                Name:
+                <input 
+                    onChange={(event) => handleChange(event)}
+                    name="name"
+                    type="text" 
+                    value={user.name} 
+                />
             </label>
+            <label>
+                Email:
+                <input 
+                    onChange={(event) => handleChange(event)}
+                    name="email" 
+                    type="email" 
+                    value={user.email} 
+                />
+            </label>
+            <label>
+                Role:
+                <input 
+                    onChange={(event) => handleChange(event)}
+                    name="role" 
+                    type="text" 
+                    value={user.role} 
+                />
+            </label>
+            <button>Submit!</button>
         </form>
     );   
 }
